f7e1fd4cb2420c4871917ef145be1c19e858d75f
soni17 <elb004@gmail.com>
2018-06-05 21:44:55 -0400

    add log functionality

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameisRunning: false
+    gameisRunning: false,
+    turns: []
   },
   methods: {
     startGame: function(){
       this.gameisRunning = true,
       this.playerHealth = 100,
-      this. monsterHealth = 100
+      this. monsterHealth = 100,
+      this.turns = []
     },
     attack: function(){
-      this.monsterHealth -= this.calculateDamage(3,10);
+      var damage = this.calculateDamage(3,10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if (this.checkWin()){
         return;
       }
       this.monsterAttacks();
     },
     specialAttack: function(){
-      this.monsterHealth -= this.calculateDamage(10,20);
+      var damage = this.calculateDamage(10,20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster hard for ' + damage
+      });
       if (this.checkWin()){
         return;
       }
@@ -31,14 +43,23 @@ new Vue({
       }else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
       this.monsterAttacks();
     },
     giveUp: function(){
       this.gameisRunning = false;
     },
     monsterAttacks: function(){
-      this.playerHealth -= this.calculateDamage(5,12);
+      var damage = this.calculateDamage(5,12);
+      this.playerHealth -= damage;
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits player for ' + damage
+      });
     },
     calculateDamage: function(min,max){
       return Math.max( Math.floor( Math.random() * max ) + 1 , min);
