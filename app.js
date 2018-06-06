4e987cf3c3583ecf96f147038063be4e06a62e05
soni17 <elb004@gmail.com>
2018-06-05 20:42:53 -0400

    add behavior to attack button

--- app.js
+++ app.js
@@ -10,6 +10,39 @@ new Vue({
       this.gameisRunning = true,
       this.playerHealth = 100,
       this. monsterHealth = 100
+    },
+    attack: function(){
+      var max = 10;
+      var min = 3;
+      var damage = Math.max( Math.floor( Math.random() * max ) + 1 , min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth<=0){
+        alert("you won");
+        this.gameisRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max( Math.floor( Math.random() * max ) + 1 , min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth<=0){
+        alert("you lost");
+        this.gameisRunning = false;
+        return;
+      }
+
+    },
+    specialAttack: function(){
+
+    },
+    heal: function(){
+
+    },
+    giveUp: function(){
+
     }
   }
 });
