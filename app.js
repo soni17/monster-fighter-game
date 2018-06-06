63febaf38960bde7ade72449c2f398d40ef9228e
soni17 <elb004@gmail.com>
2018-06-05 20:57:40 -0400

    refactor code for attack button

--- app.js
+++ app.js
@@ -12,27 +12,17 @@ new Vue({
       this. monsterHealth = 100
     },
     attack: function(){
-      var max = 10;
-      var min = 3;
-      var damage = Math.max( Math.floor( Math.random() * max ) + 1 , min);
+      var damage = this.calculateDamage(3,10);
       this.monsterHealth -= damage;
 
-      if (this.monsterHealth<=0){
-        alert("you won");
-        this.gameisRunning = false;
+      if (this.checkWin()){
         return;
       }
 
-      max = 12;
-      min = 5;
-      damage = Math.max( Math.floor( Math.random() * max ) + 1 , min);
+      damage = this.calculateDamage(5,12);
       this.playerHealth -= damage;
 
-      if (this.playerHealth<=0){
-        alert("you lost");
-        this.gameisRunning = false;
-        return;
-      }
+      this.checkWin;
 
     },
     specialAttack: function(){
@@ -43,6 +33,27 @@ new Vue({
     },
     giveUp: function(){
 
+    },
+    calculateDamage: function(min,max){
+      return Math.max( Math.floor( Math.random() * max ) + 1 , min);
+    },
+    checkWin: function(){
+      if (this.monsterHealth<=0){
+        if (confirm('You won! New Game')){
+          this.startGame();
+        } else {
+          this.gameisRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0){
+        if (confirm("You lost! New Game?")){
+          this.startGame();
+        } else {
+          this.gameisRunning = false;
+        }
+        return true;
+      }
+      return false
     }
   }
 });
