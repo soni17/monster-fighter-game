be1e3b6abe04bb8800e96143d84722ecc7c68a2d
soni17 <elb004@gmail.com>
2018-06-05 20:28:34 -0400

    add behavior to the start new game button

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameisRunning: false
+  },
+  methods: {
+    startGame: function(){
+      this.gameisRunning = true,
+      this.playerHealth = 100,
+      this. monsterHealth = 100
+    }
   }
 });
