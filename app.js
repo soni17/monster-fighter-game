903913ea68e74ab30b87524f16411119f2b8a360
soni17 <elb004@gmail.com>
2018-06-05 21:11:57 -0400

    add behaviour to special attack button

--- app.js
+++ app.js
@@ -12,20 +12,21 @@ new Vue({
       this. monsterHealth = 100
     },
     attack: function(){
-      var damage = this.calculateDamage(3,10);
-      this.monsterHealth -= damage;
-
+      this.monsterHealth -= this.calculateDamage(3,10);
       if (this.checkWin()){
         return;
       }
 
-      damage = this.calculateDamage(5,12);
-      this.playerHealth -= damage;
-
-      this.checkWin;
+    this.monsterAttacks();
 
     },
     specialAttack: function(){
+      this.monsterHealth -= this.calculateDamage(10,20);
+      if (this.checkWin()){
+        return;
+      }
+
+      this.monsterAttacks();
 
     },
     heal: function(){
@@ -34,12 +35,16 @@ new Vue({
     giveUp: function(){
 
     },
+    monsterAttacks: function(){
+      this.playerHealth -= this.calculateDamage(5,12);
+      this.checkWin();
+    },
     calculateDamage: function(min,max){
       return Math.max( Math.floor( Math.random() * max ) + 1 , min);
     },
     checkWin: function(){
-      if (this.monsterHealth<=0){
-        if (confirm('You won! New Game')){
+      if (this.monsterHealth <= 0){
+        if (confirm('You won! New Game?')){
           this.startGame();
         } else {
           this.gameisRunning = false;
